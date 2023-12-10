1efcf871f6dbf7523a545bc61ec0f3e1bfc998f7
ZeRo <jawhaa.jawhaa2003@gmail.com>
2023-12-10 12:35:18 +0800

    Base code with react and laravel

---
+++ resources/js/Pages/Component/test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function test(){
+    return (
+        <div className="container">
+            <h1 className="display-4">TEstt2</h1>
+            <p className="lead">
+                Za ymarch bsn inged heden huudsaa gargachihin bn
+            </p>
+            <button className="btn btn-primary">TPrimary tooovch</button>
+        </div>
+    );
+}
